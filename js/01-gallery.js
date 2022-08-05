bf114b5b760a2f8076b3a50610f39a74399a690f
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-05 20:47:00 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
